d10928aecee67b3c8865465432f1c96848019937
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2022-01-15 17:49:05 -0300

    add types for kinds

---
+++ mlog/kinds.d.ts
@@ -0,0 +1,35 @@
+/// <reference path="./traits.d.ts" />
+
+interface BasicUnit
+  extends Shooting,
+    WithHealth,
+    Rotatable,
+    Spaced,
+    Ranged,
+    Typed,
+    Flagged,
+    Controllable,
+    Nameable {}
+
+interface AnyUnit extends BasicUnit, Boosted {}
+
+interface BasicBuilding
+  extends LiquidHolder,
+    ItemHolder,
+    PowerHolder,
+    WithHealth,
+    WithEffiency,
+    WithTimescale,
+    Rotatable,
+    Spaced,
+    Ranged,
+    Typed,
+    WithEnable,
+    WithConfig {}
+
+interface AnyBuilding extends BasicBuilding, Heatable, WithProgress {}
+
+interface BasicTurret extends BasicBuilding, Shooting, WithProgress {}
+
+// just to make this future proof
+interface AnyTurret extends BasicTurret {}
