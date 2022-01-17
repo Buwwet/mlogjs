be9955b072065d285cf277a0d6ed9af03f327c73
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2022-01-17 17:39:33 -0300

    add sensor and radar instructions

--- src/instructions/blocks.ts
+++ src/instructions/blocks.ts
@@ -1,5 +1,6 @@
 import { IReadable, IWritable } from "../values/value";
 import { IInstruction } from "./instruction";
+import { TRadarFilter, TRadarFilterArray, TRadarSort } from "./units";
 
 export class DrawFlushInstruction implements IInstruction<"drawflush"> {
   readonly name = "drawflush";
@@ -57,5 +58,37 @@ export class ControlInstruction implements IInstruction<"control"> {
   toMlog(): string {
     const [mode, ...values] = this.data;
     return `${this.name} ${mode} ${values.map(({ value }) => value).join(" ")}`;
+  }
+}
+
+export class RadarInstruction implements IInstruction<"radar"> {
+  readonly name = "radar";
+
+  constructor(
+    public from: IReadable,
+    public filter: TRadarFilterArray,
+    public order: boolean,
+    public sort: TRadarSort,
+    public output: IWritable
+  ) {}
+
+  toMlog(): string {
+    const [f1, f2 = "any", f3 = "any"] = this.filter;
+
+    return `${this.name} ${f1} ${f2} ${f3} ${this.sort} ${this.from.value} ${this.order} ${this.output.identifier}`;
+  }
+}
+
+export class SensorInstruction implements IInstruction<"sensor"> {
+  readonly name = "sensor";
+
+  constructor(
+    public type: IReadable,
+    public target: IReadable,
+    public output: IWritable
+  ) {}
+
+  toMlog(): string {
+    return `${this.name} ${this.output.identifier} ${this.target.value} ${this.type.value}`;
   }
 }
