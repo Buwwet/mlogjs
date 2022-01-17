52267b94f685f44fe7a8fcd3b548be3a46002b44
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2022-01-17 16:37:16 -0300

    add an index export for the lib

---
+++ lib/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="./commands.d.ts" />
+/// <reference path="./globals.d.ts" />
+/// <reference path="./kinds.d.ts" />
+/// <reference path="./memory.d.ts" />
+/// <reference path="./traits.d.ts" />
