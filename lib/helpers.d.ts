78c6d4c1a1cbc1441c97904a129f58fa5731ecd1
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2022-01-21 13:34:45 -0300

    update getVar documentation

--- lib/helpers.d.ts
+++ lib/helpers.d.ts
@@ -1,10 +1,21 @@
-/**
- * Allows using special processor variables such as `@this`, `@unit` and `@time`
- * @param name The name of the variable
- */
+/// <reference path="./kinds.d.ts" />
 
+/**
+ * Allows getting unlisted processor variables or symbols.
+ *
+ * Using this method is discouraged because most values are
+ * already accessible under the namespaces `Units`, `Blocks`,
+ * `Items`, `Liquids`, `ControlKind`, `Vars` and `UnitCommands`.
+ *
+ * @param key The identifier of the value
+ */
 declare function getVar<T>(key: string): T;
 
+/**
+ * Gets a building linked to the processor by its name.
+ *
+ * @param name
+ */
 declare function getBuilding<T extends BasicBuilding = AnyBuilding>(
   name: string
 ): T;
