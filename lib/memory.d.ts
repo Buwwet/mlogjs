432e4ffb98e0b739ad8bddfb284a25f46210c006
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2022-02-28 20:16:15 -0300

    improvements to the Memory class definition

--- lib/memory.d.ts
+++ lib/memory.d.ts
@@ -1,3 +1,5 @@
+/// <reference path="./kinds.d.ts" />
+
 type MemoryCapacity = 64 | 512;
 /**
  * A wrapper around memory cells/banks that is compatible with custom types.
@@ -9,7 +11,7 @@ declare class Memory<
 > {
   /** The size of the memory cell/bank */
   readonly size: S;
-  constructor(size?: S);
+  constructor(cell: BasicBuilding, size?: S);
 
   /** Returns a raw view of the underying data
    *
@@ -20,7 +22,7 @@ declare class Memory<
    *    x: number;
    *    y: number;
    *  }
-   *  const cell = new Memory<Point>();
+   *  const cell = new Memory<Point>(getBuilding("cell1"));
    *  cell[0].x = 10;
    *  cell[0].y = 50;
    *
@@ -29,7 +31,7 @@ declare class Memory<
    *  print(rawCell[0]); // 10
    *  print(rawCell[1]); // 50
    */
-  raw(): Memory<number, S>;
+  raw(): RawMemory<S>;
   [Symbol.iterator](): IterableIterator<T>;
   [index: number]: T;
 
@@ -39,4 +41,12 @@ declare class Memory<
    * the `size` of the cell divided by the total size required by the type.
    */
   readonly length: L;
+}
+
+declare class RawMemory<S extends MemoryCapacity = 64> extends Memory<
+  number,
+  S,
+  S
+> {
+  constructor(memory: Memory);
 }
